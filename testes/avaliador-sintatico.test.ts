95b25a16a458ed8782d536833a5f01546acbcce3
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-04-23 16:52:16 -0700

    Atualização do núcleo de Delégua para a versão 0.41.0.

--- testes/avaliador-sintatico.test.ts
+++ testes/avaliador-sintatico.test.ts
@@ -333,7 +333,7 @@ describe('Avaliador Sintático Birl', () => {
                 expect(retornoAvaliadorSintatico.declaracoes[0].assinaturaMetodo).toBe('<principal>');
                 expect(retornoAvaliadorSintatico.declaracoes[0]).toBeInstanceOf(FuncaoDeclaracao);
                 const declaracoes = retornoAvaliadorSintatico.declaracoes[0] as FuncaoDeclaracao;
-                expect(declaracoes.tipoRetorno).toBe('MONSTRO');
+                expect(declaracoes.tipo).toBe('MONSTRO');
             });
 
             it('Sucesso - declaração - chamarFuncao - string', () => {
